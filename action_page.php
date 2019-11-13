3965c1a82400c60a43ad9a9a267e93ab00b58094
Scammaz <44886383+Scammaz@users.noreply.github.com>
2019-11-13 14:30:29 -0500

    actionpage

--- action_page.php
+++ action_page.php
@@ -3,14 +3,13 @@
 <head>
 <title> Yeet  </title>
         <link rel="stylesheet" type="text/css" href="stylemaine.css"> <!--external css-->
-        <script src="validate/checkEmptyForm.js"> </script> <!--external javascript form check-->
-        <script src="validate/validateName.js"></script> <!--external javascript name check-->  
+
 </head>
 
 <body>
 
-Welcome <?php echo $_POST["first","last"] ; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Welcome <?php echo $_GET["first"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
 
 </body>
 </html>
