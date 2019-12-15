71f4f590c6e033e700dba8d83279d24d3dfe72f5
Scammaz <44886383+Scammaz@users.noreply.github.com>
2019-12-15 00:12:03 -0500

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -8,14 +8,14 @@
 
 <body>
 <?php
-$email = ($_GET"email")
-$first =($_GET"first" )
-$last =($_GET"last" )
-$email = filter_var($email, FILTER_SANITIZE_EMAIL)
-$first = filter_var($first, FILTER_SANITIZE_STRING)
-$last = filter_var($last, FILTER_SANITIZE_STRING)
-echo("Welcome $first, $last")
-echo("Your Email Address is: $email" )
+$email = ($_GET"email");
+$first =($_GET"first" );
+$last =($_GET"last" );
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+$first = filter_var($first, FILTER_SANITIZE_STRING);
+$last = filter_var($last, FILTER_SANITIZE_STRING);
+echo("Welcome $first, $last");
+echo("Your Email Address is: $email" );
 
 ?>
 
