2aa2ea114b59c1c9bfed34b4377c086064846289
Scammaz <44886383+Scammaz@users.noreply.github.com>
2019-12-15 15:49:21 -0500

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -23,5 +23,33 @@ $last = filter_var($last, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     echo "<br>";
 ?>
 
+<?php
+$servername = "localhost";
+$username = "php";
+$password = "password";
+$dbname = "test";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT userid, firstname, lastname, email FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "ID: " . $row["userid"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. " Email:" . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
+
+
 </body>
 </html>
