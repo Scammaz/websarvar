e637a8e730ad0f232e37d094a23014d74facaa72
Scammaz <44886383+Scammaz@users.noreply.github.com>
2019-12-14 22:53:40 -0500

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -7,14 +7,6 @@
 </head>
 
 <body>
-
-Welcome <?=$first. " " . $last;?> <br>
-Your email address is: <?php echo $email; ?>
-
-
-</body>
-</html>
-
 <?php
 $email = ($_GET"email")
 $first =($_GET"first" )
@@ -22,12 +14,10 @@ $last =($_GET"first" )
 $email = filter_var($email, FILTER_VALIDATE_EMAIL)
 $first = filter_var($first, FILTER_VALIDATE_STRING)
 $last = filter_var($last, FILTER_VALIDATE_STRING)
+echo("Welcome "$first, $last)
+echo("Your Email Address is:", $email)
+
 ?>
-<?php                       
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
+
+</body>
+</html>
