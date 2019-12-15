b076c26da92ac586923a64f5c5143960ef82f922
Scammaz <44886383+Scammaz@users.noreply.github.com>
2019-12-15 13:21:30 -0500

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -11,12 +11,13 @@
 $email = ($_GET['email']);
 $first =($_GET['first']);
 $last =($_GET['last']);
+
 $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-$first = filter_var($first, FILTER_SANITIZE_STRING);
-$last = filter_var($last, FILTER_SANITIZE_STRING);
-echo $email 
-echo $first
-echo $last
+#$first = filter_var($first, FILTER_SANITIZE_STRING);
+#$last = filter_var($last, FILTER_SANITIZE_STRING);
+    echo ( "$email is valid email");
+ #   echo ("First name = $first");
+ #   echo ("LAST NAME = $last");
 
 ?>
 
